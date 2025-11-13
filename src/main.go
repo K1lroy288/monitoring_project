fc30a90227271d7436a1575722ea573848f3f5bb
K1lroy <k1lroyd@mail.ru>
2025-11-13 09:52:20 +0300

    add cpu load metric

--- src/main.go
+++ src/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/shirou/gopsutil/v3/cpu"
 )
 
 var httpRequestsTotal = prometheus.NewCounterVec(
@@ -15,13 +18,33 @@ var httpRequestsTotal = prometheus.NewCounterVec(
 	[]string{"path"},
 )
 
+var cpuLoaded = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "CPU_loaded",
+	},
+)
+
+func cpuLoad(cpuL prometheus.Gauge) {
+	for {
+		percentages, err := cpu.Percent(200*time.Millisecond, false)
+		if err != nil {
+			log.Fatalf("Не удалосось получить загруженность CPU: %v", err)
+		} else {
+			cpuL.Set(percentages[0])
+		}
+		time.Sleep(10 * time.Second)
+	}
+}
+
 func main() {
-	prometheus.MustRegister(httpRequestsTotal)
+	prometheus.MustRegister(httpRequestsTotal, cpuLoaded)
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		httpRequestsTotal.WithLabelValues(r.URL.Path).Inc()
 		w.Write([]byte("Service is up!"))
 	})
+
+	go cpuLoad(cpuLoaded)
 
 	http.Handle("/metrics", promhttp.Handler())
 
